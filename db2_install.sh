a44f1f40d692159d920b0e0c14931c30fc19a037
modernix <tranb@nova.local>
2014-08-31 21:06:23 +0200

    Original

---
+++ db2_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#install required files 
+#yum install libaio.so.1 libpam.so.0 -y
+yum install libaio numactl libpam.so.0 -y
+
+#disable ASLR
+#echo "kernel.randomize_va_space=0" >> /etc/sysctl.conf
+#sysctl -p
+
+#install db2
+cd /vagrant
+#tar -zxvf v9.7fp9_linuxx64_server.tar.gz --owner root --group root --no-same-owner 2>&1 > tar2.log
+tar -zxvf v9.7fp9_linuxx64_server.tar.gz --owner root --group root --no-same-owner
+/vagrant/server/db2setup -r /vagrant/db2ese.rsp
