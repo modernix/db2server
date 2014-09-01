57856926dae8253f28fcd4082c36199e6c558f43
modernix <tranb@nova.local>
2014-09-01 22:39:32 +0200

    remove sume unnecessary lines

--- db2_install.sh
+++ db2_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #install required files 
-#yum install libaio.so.1 libpam.so.0 -y
 yum install libaio numactl libpam.so.0 -y
 
 #disable ASLR -
@@ -10,7 +9,6 @@ sysctl -p
 
 #install db2
 cd /vagrant
-#tar -zxvf v9.7fp9_linuxx64_server.tar.gz --owner root --group root --no-same-owner 2>&1 > tar2.log
 tar -zxvf v9.7fp9_linuxx64_server.tar.gz --owner root --group root --no-same-owner
 /vagrant/server/db2setup -r /vagrant/db2ese.rsp
 
